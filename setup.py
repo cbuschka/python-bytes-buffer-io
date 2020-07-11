f2cf226dad9cdcfe3e1d9add4b4225098150b5e2
conni <cbuschka@gmail.com>
2020-07-11 12:44:14 +0200

    Changed links in pypi readme to absolute ones.

--- setup.py
+++ setup.py
@@ -1,22 +1,28 @@
 import re
-import setuptools
 import subprocess
 
-def get_readme():
+import setuptools
+
+
+def get_readme(version):
     with open("readme.md", "r", encoding='utf-8') as fh:
         readme = fh.read()
+        readme = re.sub(r"]\(\./", f"](https://github.com/cbuschka/python-bytesbufio/blob/v{version}/", readme)
         return readme
 
+
 def get_version():
-    proc = subprocess.Popen(['git', 'describe', '--dirty', '--tags'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    proc = subprocess.Popen(['git', 'describe', '--exact-match', '--tags'], stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT)
     stdout, stderr = proc.communicate()
     result = re.search('^v([^\n]+)\n$', stdout.decode("utf-8"), re.S)
     if not result:
         raise ValueError("Invalid version: '{}'.".format(result))
     return result.group(1)
 
+
 version = get_version()
-long_description = get_readme()
+long_description = get_readme(version)
 
 setuptools.setup(
     name="bytesbufio",
